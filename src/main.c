0bcc6007e60153e285cf49d0b698f1466c25ee10
James Deery <james.deery@jadex.org>
2013-03-24 20:27:45 +0000

    Fix hamiltoncli

--- src/main.c
+++ src/main.c
@@ -10,18 +10,16 @@
 #include "hamilton/lib.h"
 #include "hamilton/audio.h"
 #include "hamilton/midi.h"
+#include "hamilton/core_synths.h"
 
-int sine_wave_register(void);
-int mda_dx10_register(void);
-
-static int run()
+static int run(HmBand *band)
 {
 	int error;
 
 	error = hm_midi_init();
 	if (error) goto end;
 
-	error = hm_audio_init();
+	error = hm_audio_init(band);
 	if (error) goto end;
 
 	hm_audio_start();
@@ -39,19 +37,19 @@ static int run()
 
 				switch (type) {
 					case 0x8:
-						hm_band_send_note(0, channel, false, data1, data2 / 127.0);
+						hm_band_send_note(band, 0, channel, false, data1, data2 / 127.0);
 						break;
 
 					case 0x9:
-						hm_band_send_note(0, channel, true, data1, data2 / 127.0);
+						hm_band_send_note(band, 0, channel, true, data1, data2 / 127.0);
 						break;
 
 					case 0xB:
-						hm_band_send_cc(0, channel, data1, data2 / 127.0);
+						hm_band_send_cc(band, 0, channel, data1, data2 / 127.0);
 						break;
 
 					case 0xC:
-						hm_band_send_patch(0, channel, data1);
+						hm_band_send_patch(band, 0, channel, data1);
 						break;
 				}
 			}
@@ -72,25 +70,26 @@ int main(int argc, char *argv[])
 {
 	int error;
 
-	error = hm_band_init();
+	HmBand *band = NULL;
+	error = hm_band_init(&band);
 	if (error) goto end;
 
-	error = sine_wave_register();
+	error = sine_wave_register(band);
 	if (error) goto end;
 
-	error = mda_dx10_register();
+	error = mda_dx10_register(band);
 	if (error) goto end;
 
 	int n;
-	const HmSynthType *synths = hm_lib_get_synths(&n);
+	const HmSynthType *synths = hm_lib_get_synths(hm_band_get_lib(band), &n);
 
-	error = hm_band_set_channel_synth(0, &synths[1]);
+	error = hm_band_set_channel_synth(band, 0, &synths[1]);
 	if (error) goto end;
 
-	error = run();
+	error = run(band);
 
 end:
-	hm_band_free();
+	hm_band_free(band);
 
 	return error;
 }
