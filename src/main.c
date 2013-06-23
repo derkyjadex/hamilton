2e2bba91a9ba62564de857215f894a04eb8594c4
James Deery <james.deery@jadex.org>
2013-06-23 19:57:45 +0100

    Run a script to control the band

--- src/main.c
+++ src/main.c
@@ -15,55 +15,6 @@
 #include "albase/lua.h"
 #include "albase/script.h"
 
-static int run(HmBand *band)
-{
-	BEGIN()
-
-	TRY(hm_midi_init());
-	TRY(hm_audio_init(band));
-
-	hm_audio_start();
-
-	bool finished = false;
-
-	while (!finished) {
-		{
-			uint8_t status, data1, data2;
-			while (hm_midi_read(&status, &data1, &data2) > 0) {
-				uint8_t type = status >> 4;
-				uint8_t channel = status & 0x0F;
-
-				printf("%x, %x, %x, %x\n", type, channel, data1, data2);
-
-				switch (type) {
-					case 0x8:
-						hm_band_send_note(band, 0, channel, false, data1, data2 / 127.0);
-						break;
-
-					case 0x9:
-						hm_band_send_note(band, 0, channel, true, data1, data2 / 127.0);
-						break;
-
-					case 0xB:
-						hm_band_send_cc(band, 0, channel, data1, data2 / 127.0);
-						break;
-
-					case 0xC:
-						hm_band_send_patch(band, 0, channel, data1);
-						break;
-				}
-			}
-		}
-
-		SDL_Delay(4);
-	}
-
-	PASS(
-		hm_audio_free();
-		hm_midi_free();
-	)
-}
-
 #undef main
 int main(int argc, char *argv[])
 {
@@ -73,20 +24,33 @@ int main(int argc, char *argv[])
 	lua_State *L = NULL;
 
 	TRY(hm_band_init(&band));
-	TRY(al_script_init(&L));
-	hm_load_cmds(L, band);
 
 	TRY(sine_wave_register(band));
 	TRY(mda_dx10_register(band));
 
-	int n;
-	const HmSynthType *synths = hm_lib_get_synths(hm_band_get_lib(band), &n);
+	TRY(hm_audio_init(band));
+	hm_audio_start();
 
-	TRY(hm_band_set_channel_synth(band, 0, &synths[1]));
+	TRY(al_script_init(&L));
+	hm_load_cmds(L, band);
 
-	TRY(run(band));
+	for (int i = 1; i < argc; i++) {
+		TRY(al_script_run_file(L, argv[i]));
+	}
+
+	while (true) {
+		lua_getglobal(L, "frame");
+		if (!lua_isnil(L, -1)) {
+			lua_call(L, 0, 0);
+		} else {
+			lua_pop(L, 1);
+		}
+
+		SDL_Delay(10);
+	}
 
 	PASS(
+		hm_audio_free();
 		hm_band_free(band);
 		lua_close(L);
 	)
